da63c0e2a0ad876018a7edbe4157728ab9801e94
alexChiang <alexchiang.cg@gmail.com>
2025-05-08 21:54:47 +0800

    refactor: 分開實作範例

--- src/ch05_3.ts
+++ src/ch05_3.ts
@@ -15,17 +15,22 @@
 type Info2 = {}
 
 function getData(data: string): string[]
-function getData(data: number): number
+
 function getData(data: boolean): boolean
 function getData(data: Info2): Info2
 function getData(data: unknown): unknown {
-  console.log();
-  if (typeof data === 'string') return []
-  if (typeof data === 'number') return 999
-  // 判斷 Info2
-  // return 某個data
+
   return false
 }
+
+function getData(data: number): number {
+  return 999
+}
+
+function getData(data: string): string[] {
+  return []
+}
+
 
 getData(99)
 getData('')

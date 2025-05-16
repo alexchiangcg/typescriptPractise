b93cba24de2e7680603e4014a4f5f459739e1c56
alexChiang <alexchiang.cg@gmail.com>
2025-05-16 16:04:34 +0800

    feat: add examples of extends and infer

---
+++ src/ch07_4.ts
@@ -0,0 +1,29 @@
+// 在infer之前，補充extends用法 (Array)
+// function sliceArr<T>(a: T) {
+//   console.log(a.length)
+// }
+
+function sliceArr<T extends Array<number>>(a: T) {
+    console.log(a.length)
+}
+
+
+
+// infer用法
+// var a = 100
+// let
+type TT1<T> = T extends Array<infer P> ? P : never;
+type R1 = TT1<['bruce', 1]>
+type RR1 = TT1<string[]>
+type RR2 = TT1<number>
+
+
+// function用法
+type ParamType<T> = T extends (param1: infer P) => any ? P : never
+
+type F1 = ParamType<(a: number) => void>
+
+interface UserCard { name: string }
+type F2 = ParamType<(a: UserCard) => void>
+
+type F3 = ParamType<[]>
